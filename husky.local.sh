f0022c54245463a289fec8f18143db48e78c32e9
shaojuanzhang <rollingcomma@gmail.com>
2020-03-17 16:18:38 -0700

    testing hook

---
+++ husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.2.3 (https://github.com/typicode/husky#readme)
+#   At: 3/17/2020, 2:39:49 PM
+#   From: /Users/shaojuanzhang/Study/BCIT/COMP2340_Tools/labs/lab6/proj/.git/hooks/node_modules/husky (https://github.com/typicode/husky#readme)
+
+packageManager=npm
+cd "."
---
+++ test/hook.test.js
@@ -0,0 +1,7 @@
+test("ugly", () => {
+    expect(4).toBe(4);
+});
+
+test("failing", () => {
+    expect(4).toBe(5);
+});
